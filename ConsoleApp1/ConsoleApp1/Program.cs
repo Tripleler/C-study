e25baa906913a4b3052a64e92ea131645551c429
Tripleler <86835527+Tripleler@users.noreply.github.com>
2023-04-07 17:19:16 +0900

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace CS
+{
+    class CodingTest
+    {
+        public int Calculate(string[] tokens)
+        {
+            Stack<int> stack = new Stack<int>();
+            try
+            {
+                foreach (string token in tokens)
+                {
+                    switch (token)
+                    {
+                        case "+": stack.Push(stack.Pop() + stack.Pop()); break;
+                        case "-": stack.Push(-(stack.Pop() - stack.Pop())); break;
+                        case "*": stack.Push(stack.Pop() * stack.Pop()); break;
+                        case "/":
+                            int rv = stack.Pop();
+                            stack.Push(stack.Pop() / rv); break;
+                        default: stack.Push(int.Parse(token)); break;
+                    }
+                }
+                return stack.Pop();
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodingTest ct = new CodingTest();
+            int value = ct.Calculate(new string[] { "2", "3", "5", "*", "+", "7", "-" });
+            Console.WriteLine(value);
+        }
+    }
+}
